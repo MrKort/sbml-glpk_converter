2f5bc07430fa86bbc2eb689b6ca77a28b4543168
MrKort <kevinkort@hotmail.com>
2017-12-08 16:18:59 +0100

    made new functions in Bacterium.h and added comments

--- source_code/bacterium.h
+++ source_code/bacterium.h
@@ -11,8 +11,11 @@
 LIBSBML_CPP_NAMESPACE_USE
 
 class Bacterium {
+//##############################//
+//      CLASS DEFINITION        //
+//##############################//
 public:
-    void readInputSBML(const char *&);
+    void readFileSBML(const char *&);
     void doFBA();
     void outputMatrix();
 
@@ -22,6 +25,9 @@ private:
 
     //Index associated with the objective function reaction (biomass production)
     unsigned int ObjFunc;
+
+    //Total amount of forward and reverse reactions
+    unsigned int fwdCount;
     unsigned int revCount;
 
     //Declare internal and external maps to link SBML metabolite name to GLPK metabolite index
@@ -35,141 +41,153 @@ private:
     //Delare vectors to be used by GLPK
     std::vector<int> vecMeta, vecReac;          //vector with GLPK row and column index values
     std::vector<double> vecStoi;                //vector with GLPK stoichiometry values
+
+    //void functions used by readFileSBML()
+    void readMetaSBML(const ListOfSpecies *&);
+    void readReacSBML(const ListOfReactions *&);
+    void initMatrix(const unsigned int &, const bool &, const std::string &, const std::string &, const double &);
+
 };
 
+//#################################################################################################
 
-void Bacterium::readInputSBML(const char *&inputFile){
+void Bacterium::readFileSBML(const char *&inputFile){
 //##############################//
 //  SBML FILE READING FUNCTION  //
 //##############################//
 
-        //Read test input file with SBML
+//Open SBML input file
         SBMLDocument *document = readSBML(inputFile);
-
+//Check for errors in SBML file
         unsigned int errors = document->getNumErrors();
         if(errors){
             std::string errorMessage = std::string("could not read SBML file: ") + inputFile;
             throw std::string(errorMessage);
         }
 
+//Extract SBML model from file
         Model *model = document->getModel();
         speciesID = model->getId();
 
-        ListOfSpecies *spList = model->getListOfSpecies();
-        ListOfReactions *reacList = model->getListOfReactions();
+//Read metabolite data from SBML model
+        const ListOfSpecies *spList = model->getListOfSpecies();
+        readMetaSBML(spList);
 
-        //Read metabolite data from SBML file
-        for(unsigned int i = 0u, metaCount = 0u; i < spList->size(); ++i) {
-            std::string spId = spList->get(i)->getId();
-            std::string spComp = spId.substr(spId.size()-1, spId.size());
-
-            //Store metabolite name and number in map
-            if(spComp == "e") {
-                metaName[metaCount] = spId; //compartmentalisation independent storage map
-                environment[spId] = metaCount++;
-            }
-            else if(spComp == "c") {
-                metaName[metaCount] = spId; //compartmentalisation independent storage map
-                cytosol[spId] = metaCount++;
-            }
-            else {
-                std::string errorMessage = std::string("unexpected compartment whilst reading SBML file: ") + inputFile;
-                throw std::string (errorMessage);
-            }
-        }
+//Check if biomass is present as a cytosolic metabolite
         if(!cytosol.find("M_biomass_c")->second) {
             throw std::string ("could not find a biomass metabolite in cytosol compartment.");
         }
 
-        //These counters are initialised at 1, because glp can't handle 0's
-        unsigned int matrixCount = 1u;
+//Read reaction data from SBML model & generate initial GLPK matrix
+        const ListOfReactions *reacList = model->getListOfReactions();
+        fwdCount = reacList->size();
         revCount = 1u;
+        readReacSBML(reacList);
 
-        //Write substrates and products from SMBL files into GLPK arrays
-        for(unsigned int i = 0u; i < reacList->size(); ++i) {
-            const Reaction *reac = reacList->get(i);
-            const std::string reacId = reac->getId();
-            reacName[i] = reacId;
 
-            //Determine wheter the reaction name is biomass0, this is usually the objective function
-            //In the future update the objective function determenation by using SBML::FBC:objective_function
-            if(reac->getName() == "biomass0") {
-                ObjFunc = i;
-            }
-
-            //For loop for substrates
-            unsigned int m = reac->getListOfReactants()->size();
-            for(unsigned int j = 0u; j < m; ++j) {
-                const std::string subId = reac->getReactant(j)->getSpecies();
-                const std::string subComp = subId.substr(subId.size()-1, subId.size());
-                const double subStoi = reac->getReactant(j)->getStoichiometry();
-
-                //Build stoichiometry matrix using maps.find() to get metabolite number
-                if(subComp == "e") {
-                    const int subNum = environment.find(subId)->second;
-                    vecMeta.push_back(subNum+1), vecReac.push_back(i+1), vecStoi.push_back(-subStoi);
-                    ++matrixCount;
-                    if(reac->getReversible()) { //check if reaction is reversible, if so create the reverse reaction
-                        vecMeta.push_back(subNum+1), vecReac.push_back(reacList->size() + revCount), vecStoi.push_back(+subStoi);
-                        ++matrixCount;
-                    }
-                }
-                else if(subComp == "c") {
-                    const int subNum = cytosol.find(subId)->second;
-                    vecMeta.push_back(subNum+1), vecReac.push_back(i+1), vecStoi.push_back(-subStoi);
-                    ++matrixCount;
-                    if(reac->getReversible()) { //check if reaction is reversible, if so create the reverse reaction
-                        vecMeta.push_back(subNum+1), vecReac.push_back(reacList->size() + revCount), vecStoi.push_back(+subStoi);
-                        ++matrixCount;
-                    }
-                }
-                else {
-                    std::string errorMessage = std::string("unexpected compartment whilst reading substrates from SBML file: ") + inputFile;
-                    throw std::string (errorMessage);
-                }
-            }
-
-            //For loop for products
-            m = reac->getListOfProducts()->size();
-            for(unsigned int j = 0u; j < m; ++j) {
-                const std::string prodId = reac->getProduct(j)->getSpecies();
-                const std::string prodComp = prodId.substr(prodId.size()-1, prodId.size());
-                const double prodStoi = reac->getProduct(j)->getStoichiometry();
-
-                //Build stoichiometry matrix using maps.find() to get metabolite number
-                if(prodComp == "e") {
-                    const int prodNum = environment.find(prodId)->second;
-                    vecMeta.push_back(prodNum+1), vecReac.push_back(i+1), vecStoi.push_back(+prodStoi);
-                    ++matrixCount;
-                    if(reac->getReversible()) { //check if reaction is reversible, if so create the reverse reaction
-                        vecMeta.push_back(prodNum+1), vecReac.push_back(reacList->size() + revCount), vecStoi.push_back(-prodStoi);
-                        ++matrixCount;
-                    }
-                }
-                else if(prodComp == "c") {
-                    const int prodNum = cytosol.find(prodId)->second;
-                    vecMeta.push_back(prodNum+1), vecReac.push_back(i+1), vecStoi.push_back(+prodStoi);
-                    ++matrixCount;
-                    if(reac->getReversible()) { //check if reaction is reversible, if so create the reverse reaction
-                        vecMeta.push_back(prodNum+1), vecReac.push_back(reacList->size() + revCount), vecStoi.push_back(-prodStoi);
-                        ++matrixCount;
-                    }
-                }
-                else {
-                    std::string errorMessage = std::string("unexpected compartment whilst reading products from SBML file: ") + inputFile;
-                    throw std::string (errorMessage);
-                }
-            }
-            if(reac->getReversible()) {
-                reacName[reacList->size() + revCount - 1] = reacId + "_reverse";
-                ++revCount;
-            }
-        }
-
-        //Done reading file, now delete the SBML document variable
+//Delete SBML document variable
         delete document;
         std::cout << "Succesfully read data from SBML input file: " << inputFile
                   << "\nMetabolic data for species: " << speciesID << " stored.\n\n";
+}
+
+void Bacterium::readMetaSBML(const ListOfSpecies *&spList){
+//###############################//
+// SBML METABOLITE READ FUNCTION //
+//###############################//
+    for(unsigned int metaCount = 0u; metaCount < spList->size(); ++metaCount) {
+        std::string spId = spList->get(metaCount)->getId();
+        std::string spComp = spId.substr(spId.size()-1, spId.size());
+
+//Store metabolite name and number in environment or cytosol map
+        if(spComp == "e") {
+            metaName[metaCount] = spId; //compartmentalisation independent storage map
+            environment[spId] = metaCount;
+        }
+        else if(spComp == "c") {
+            metaName[metaCount] = spId; //compartmentalisation independent storage map
+            cytosol[spId] = metaCount;
+        }
+        else {
+            std::string errorMessage = std::string("unexpected compartment whilst reading SMBL file for species: ") + speciesID;
+            throw std::string (errorMessage);
+        }
+    }
+}
+
+void Bacterium::readReacSBML(const ListOfReactions *&reacList){
+//##############################//
+// SBML REACTION READ FUNCTION  //
+//##############################//
+    for(unsigned int reacCount = 0u; reacCount < fwdCount; ++reacCount) {
+        const Reaction *reac = reacList->get(reacCount);
+        const bool reacRev = reac->getReversible();
+        const std::string reacId = reac->getId();
+        reacName[reacCount] = reacId;
+
+//Determine whether the reaction name is biomass0, this is usually the objective function
+        //In the future update the objective function determination by using SBML::FBC:objective_function
+        if(reac->getName() == "biomass0") {
+            ObjFunc = reacCount; //store index number of the "objective function"
+        }
+
+//For loop for substrates
+        unsigned int m = reac->getListOfReactants()->size();
+        for(unsigned int j = 0u; j < m; ++j) {
+            const std::string subId = reac->getReactant(j)->getSpecies();
+            const std::string subComp = subId.substr(subId.size()-1, subId.size());
+            const double subStoi = - reac->getReactant(j)->getStoichiometry();
+
+            //Build stoichiometry matrix using maps.find() to get metabolite number
+            initMatrix(reacCount, reacRev, subId, subComp, subStoi);
+        }
+
+//For loop for products
+        m = reac->getListOfProducts()->size();
+        for(unsigned int j = 0u; j < m; ++j) {
+            const std::string prodId = reac->getProduct(j)->getSpecies();
+            const std::string prodComp = prodId.substr(prodId.size()-1, prodId.size());
+            const double prodStoi = + reac->getProduct(j)->getStoichiometry();
+
+            //Build stoichiometry matrix using maps.find() to get metabolite number
+            initMatrix(reacCount, reacRev, prodId, prodComp, prodStoi);
+        }
+//Store name and index for reverse reactions
+        if(reacRev) {
+            reacName[fwdCount + (revCount - 1)] = reacId + "_reverse";
+            ++revCount;
+        }
+    }
+}
+
+void Bacterium::initMatrix(const unsigned int &reacCount, const bool &reacRev,
+                             const std::string &Id, const std::string &Comp, const double &Stoi){
+//##############################//
+//  GLPK MATRIX INITIALISATION  //
+//##############################//
+    if(Comp == "e") {
+//Check if the compartment is environment
+        const int Num = environment.find(Id)->second;
+        vecMeta.push_back(Num + 1), vecReac.push_back(reacCount + 1), vecStoi.push_back(+Stoi);
+        if(reacRev) {
+            //check if reaction is reversible, if so create the reverse reaction
+            vecMeta.push_back(Num + 1), vecReac.push_back(fwdCount + revCount), vecStoi.push_back(-Stoi);
+        }
+    }
+    else if(Comp == "c") {
+//Check if the compartment is cytosol
+        const int Num = cytosol.find(Id)->second;
+        vecMeta.push_back(Num + 1), vecReac.push_back(reacCount + 1), vecStoi.push_back(+Stoi);
+        if(reacRev) {
+            //check if reaction is reversible, if so create the reverse reaction
+            vecMeta.push_back(Num + 1), vecReac.push_back(fwdCount + revCount), vecStoi.push_back(-Stoi);
+        }
+    }
+    else {
+//Otherwise throw an error
+        std::string errorMessage = std::string("unexpected compartment whilst reading SMBL file for species: ") + speciesID;
+        throw std::string (errorMessage);
+    }
 }
 
 void Bacterium::doFBA(){
@@ -177,22 +195,22 @@ void Bacterium::doFBA(){
 // GLPK READ AND SOLVE FUNCTION //
 //##############################//
 
-//CREATE A NEW GLPK PROBLEM
+//Create new GLPK problem
             glp_prob *lp;
             lp = glp_create_prob();
             glp_set_obj_dir(lp, GLP_MAX);
 
-//ADD ROW AND COLUMNS FOR STOICHIOMETRY MATRIX
+//Add row and columns for stoichiometry matrix
             glp_add_cols(lp, reacName.size());
             glp_add_rows(lp, metaName.size());
 
-//ASSIGN METABOLITE NAME AND BOUNDS
+//Assign metabolite names and bounds
             for(unsigned int i = 0; i < metaName.size(); ++i){
                 glp_set_row_name(lp, i+1, metaName.find(i)->second.c_str());
                 glp_set_row_bnds(lp, i+1, GLP_FX, 0.0, 0.0);
             }
 
-//ASSIGN REACTION NAME AND BOUNDS
+//Assign reaction names and bounds
 
             //Need to update this depending on the environment...
             //Separate from first GLP initialisation
@@ -209,17 +227,13 @@ void Bacterium::doFBA(){
                 }
             }
 
-//LOAD AND SOLVE GLPK PROBLEM
+//Load and solve GLPK problem with Simplex method
             //vector.data() to access the internal arrays
             //vector.data()-1 to skip the first[0] element
             glp_load_matrix(lp, vecStoi.size(),vecMeta.data()-1,vecReac.data()-1,vecStoi.data()-1);
             glp_simplex(lp, NULL);
 
-//DELETE THE GLPK PROBLEM
-
-            //Is deleting the problem useful?
-            //Only need to update column bounds depending on environment...
-
+//Delete the current GLPK problem
             glp_delete_prob(lp);
             std::cout << "Succesfully finished Linear Programming step.\n\n";
 }
@@ -232,9 +246,9 @@ void Bacterium::outputMatrix(){
         std::cout << "Number of external metabolites: " << environment.size() << "\n";
         std::cout << "Number of internal metabolites: " << cytosol.size() << "\n";
         std::cout << "Total number metabolites: " << metaName.size() << "\n";
-        std::cout << "\nNumber of forward reactions: " << reacName.size() - (revCount-1) << "\n";
+        std::cout << "\nNumber of forward reactions: " << fwdCount << "\n";
         std::cout << "Number of reverse reactions: " << revCount - 1 << "\n";
-        std::cout << "Total number of reactions: " << reacName.size() << "\n";
+        std::cout << "Total number of reactions: " << fwdCount + (revCount - 1) << "\n";
         std::cout << "\nNumber of non-zeroes in sparse matrix: " << vecStoi.size() << "\n\n";
 
         //Output sparse matrix with reaction and metabolite IDs to .csv file
@@ -242,12 +256,12 @@ void Bacterium::outputMatrix(){
 //Check if Sparse_Matrices/ folder exists, otherwise make it
         std::ofstream matrix(outputFile);
         if(!matrix.is_open()) {
-            std::string errorMessage = std::string("could not open outputfile: ") + outputFile;
+            std::string errorMessage = std::string("could not open output file: ") + outputFile;
             throw std::string (errorMessage);
         }
 
         matrix << "Matrix Index\tMetabolite#\tMetaboliteID\tReaction#\tReactionID\tStoichiometry\n";
-        for(unsigned int i=1; i < vecStoi.size(); ++i){
+        for(unsigned int i = 1u; i <= vecStoi.size(); ++i){
             matrix << i << "\t" << vecMeta[i-1] << "\t" << metaName.find(vecMeta[i-1]-1)->second
                         << "\t" << vecReac[i-1] << "\t" << reacName.find(vecReac[i-1]-1)->second
                         << "\t" << vecStoi[i-1] << "\n";
--- source_code/main.cpp
+++ source_code/main.cpp
@@ -4,23 +4,27 @@
 int main (int argc, char* argv[])
 {
     try {
-        //Start of sim timer
+//Start of sim timer
         auto startTime = std::chrono::high_resolution_clock::now();
 
+//Check if arguments are given
         if(!(argc > 1)){
-            throw std::string ("no arguments (argc/argv) were given...");
+            throw std::string ("please provide at least one SBML file as argument...");
         }
 
+//Put arguments into inputFiles vector
         int bacNum = argc - 1;
         std::vector<const char*> inputFiles(bacNum);
         for(int i = 0; i < bacNum; ++i) {
             inputFiles[i] = argv[i + 1];
         }
 
+//Initialise a vector of Bacteria (size determined by the amount of arguments)
         std::vector<Bacterium> bac(bacNum);
 
+//Read SBML and do GLPK for each Bacteria
         for(int i = 0; i < bacNum; ++i) {
-            bac[i].readInputSBML(inputFiles[i]);
+            bac[i].readFileSBML(inputFiles[i]);
             bac[i].doFBA();
             bac[i].outputMatrix();
             if (i != bacNum - 1) {
@@ -28,7 +32,7 @@ int main (int argc, char* argv[])
             }
         }
 
-        //End of sim timer
+//End of sim timer
         auto endTime = std::chrono::high_resolution_clock::now();
         double simulationTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count() / 1000.0;
         std::cout << "Simulation has finished in " << simulationTime << " seconds.\n";
